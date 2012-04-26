8e3573e3553ffd6fcd4bca956bcdd3a98705c42b
Scott Leberknight <sleberkn@nearinfinity.com>
2012-04-25 23:28:08 -0400

    Add example of async API

---
+++ src/main/java/com/nearinfinity/examples/zookeeper/group/AsyncListGroup.java
@@ -0,0 +1,47 @@
+package com.nearinfinity.examples.zookeeper.group;
+
+import java.util.List;
+import java.util.concurrent.Semaphore;
+
+import org.apache.zookeeper.AsyncCallback;
+import org.apache.zookeeper.KeeperException;
+
+import com.nearinfinity.examples.zookeeper.util.ConnectionWatcher;
+
+public class AsyncListGroup extends ConnectionWatcher {
+
+    private Semaphore semaphore = new Semaphore(1);
+
+    public void list(final String groupName) throws KeeperException, InterruptedException {
+        String path = "/" + groupName;
+
+        zk.getChildren(path, false, new AsyncCallback.ChildrenCallback() {
+            @Override
+            public void processResult(int rc, String path, Object ctx, List<String> children) {
+                System.out.printf("Called back for path %s with return code %d\n", path, rc);
+                if (children == null) {
+                    System.out.printf("Group %s does not exist\n", groupName);
+                }
+                else {
+                    if (children.isEmpty()) {
+                        System.out.printf("No members in group %s\n", groupName);
+                        return;
+                    }
+                    for (String child : children) {
+                        System.out.println(child);
+                    }
+                }
+                semaphore.release();
+            }
+        }, null /* optional context object */);
+        semaphore.acquire();
+    }
+
+    public static void main(String[] args) throws Exception {
+        AsyncListGroup asyncListGroup = new AsyncListGroup();
+        asyncListGroup.connect(args[0]);
+        asyncListGroup.list(args[1]);
+        asyncListGroup.close();
+    }
+
+}
