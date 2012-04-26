0626d47c1de2444ffa52877a2852e4a30ee3cb1d
Scott Leberknight <sleberkn@nearinfinity.com>
2012-04-25 22:34:30 -0400

    Remove TODO. Does not need to be generic for this example code...

--- src/main/java/com/nearinfinity/examples/zookeeper/lock/LockWatcher.java
+++ src/main/java/com/nearinfinity/examples/zookeeper/lock/LockWatcher.java
@@ -13,7 +13,6 @@ import org.apache.zookeeper.ZooKeeper;
 
 import com.nearinfinity.examples.zookeeper.util.ConnectionHelper;
 
-// TODO Make this generic, i.e. a znode-watcher (will need to change lock-watcher.sh)
 public class LockWatcher implements Watcher {
 
     private ZooKeeper zk;
