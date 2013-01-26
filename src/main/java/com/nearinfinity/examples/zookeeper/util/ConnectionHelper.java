b55b833d46660986fd9b28bfd588509c30549874
Scott Leberknight <scott.leberknight@nearinfinity.com>
2013-01-26 16:30:04 -0500

    Move the CountDownLatch into connect() method, so that it isn't a one-time use class. As a local variable, connect() can be called multiple times and each one would return an independent ZooKeeper instance.

--- src/main/java/com/nearinfinity/examples/zookeeper/util/ConnectionHelper.java
+++ src/main/java/com/nearinfinity/examples/zookeeper/util/ConnectionHelper.java
@@ -1,19 +1,18 @@
 package com.nearinfinity.examples.zookeeper.util;
 
-import java.io.IOException;
-import java.util.concurrent.CountDownLatch;
-
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
 
 public class ConnectionHelper {
 
     public static final int DEFAULT_SESSION_TIMEOUT = 5000;
 
-    private CountDownLatch connectedSignal = new CountDownLatch(1);
-
     public ZooKeeper connect(String hosts, int sessionTimeout) throws IOException, InterruptedException {
+        final CountDownLatch connectedSignal = new CountDownLatch(1);
         ZooKeeper zk = new ZooKeeper(hosts, sessionTimeout, new Watcher() {
             @Override
             public void process(WatchedEvent event) {
