0bebcf165facd11a0fb18d582d04ed0e9b56dd53
Scott Leberknight <scott.leberknight@nearinfinity.com>
2013-01-26 15:40:57 -0500

    Add ListGroupForever class to list a group znode indefinitely (until killed, that is)

---
+++ src/main/java/com/nearinfinity/examples/zookeeper/group/ListGroupForever.java
@@ -0,0 +1,51 @@
+package com.nearinfinity.examples.zookeeper.group;
+
+import com.nearinfinity.examples.zookeeper.util.ConnectionHelper;
+import org.apache.zookeeper.*;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+
+public class ListGroupForever {
+
+    private ZooKeeper zooKeeper;
+    private Semaphore semaphore = new Semaphore(1);
+
+    public ListGroupForever(ZooKeeper zooKeeper) {
+        this.zooKeeper = zooKeeper;
+    }
+
+    public static void main(String[] args) throws Exception {
+        ZooKeeper zk = new ConnectionHelper().connect(args[0]);
+        new ListGroupForever(zk).listForever(args[1]);
+    }
+
+    public void listForever(String groupName) throws KeeperException, InterruptedException {
+        semaphore.acquire();
+        while (true) {
+            list(groupName);
+            semaphore.acquire();
+        }
+    }
+
+    public void list(String groupName) throws KeeperException, InterruptedException {
+        String path = "/" + groupName;
+
+        List<String> children = zooKeeper.getChildren(path, new Watcher() {
+            @Override
+            public void process(WatchedEvent event) {
+                if (event.getType() == Event.EventType.NodeChildrenChanged) {
+                    semaphore.release();
+                }
+            }
+        });
+        if (children.isEmpty()) {
+            System.out.printf("No members in group %s\n", groupName);
+            return;
+        }
+        Collections.sort(children);
+        System.out.println(children);
+        System.out.println("--------------------");
+    }
+}
