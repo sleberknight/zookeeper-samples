839ba1d8ff2e9c77878190f841a3efd5ff09e2c4
Scott Leberknight <scott.leberknight@nearinfinity.com>
2013-02-05 22:29:44 -0500

    Update ZK from 3.3.6 to 3.4.5. Updated EmbeddedZooKeeperServer.
    Hacked changes to Embedded ZooKeeperServer, and it appears to be slower and have
    a bunch of reconnect issues, though the tests do eventually complete OK.

--- src/test/java/com/nearinfinity/examples/zookeeper/util/EmbeddedZooKeeperServer.java
+++ src/test/java/com/nearinfinity/examples/zookeeper/util/EmbeddedZooKeeperServer.java
@@ -1,18 +1,21 @@
 package com.nearinfinity.examples.zookeeper.util;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.zookeeper.server.NIOServerCnxnFactory;
+import org.apache.zookeeper.server.ZooKeeperServer;
+import org.apache.zookeeper.server.persistence.FileTxnSnapLog;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.Random;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.zookeeper.server.NIOServerCnxn;
-import org.apache.zookeeper.server.ZooKeeperServer;
-import org.apache.zookeeper.server.persistence.FileTxnSnapLog;
-
 /**
  * Mimics what {@link org.apache.zookeeper.server.ZooKeeperServerMain} does, but provides a way to shut it down for
  * unit/integration testing purposes.
+ *
+ * TODO After upgrading to ZK 3.4.5 the tests execute slower, sometimes much slower and it looks like, from the logs,
+ *      there are a bunch of issues related to disconnects and reconnect attempts. Not sure why.
  */
 public final class EmbeddedZooKeeperServer {
 
@@ -20,8 +23,9 @@ public final class EmbeddedZooKeeperServer {
     private String dataDirectoryName;
     private boolean manageDataDir;
     private int tickTime;
-    private NIOServerCnxn.Factory cnxnFactory;
+    private NIOServerCnxnFactory cnxnFactory;
 
+    public static final int MAX_CLIENT_CONNECTIONS = 60;
     public static final int DEFAULT_TICK_TIME = 2000;
 
     public EmbeddedZooKeeperServer(int port) {
@@ -40,12 +44,13 @@ public final class EmbeddedZooKeeperServer {
         FileTxnSnapLog ftxn = new FileTxnSnapLog(dataDirectory, dataDirectory);
         zkServer.setTxnLogFactory(ftxn);
         zkServer.setTickTime(tickTime);
-        cnxnFactory = new NIOServerCnxn.Factory(new InetSocketAddress(port));
+        cnxnFactory = new NIOServerCnxnFactory();
+        cnxnFactory.configure(new InetSocketAddress(port), MAX_CLIENT_CONNECTIONS);
         cnxnFactory.startup(zkServer);
     }
 
     public void shutdown() {
-        if (cnxnFactory != null && cnxnFactory.isAlive()) {
+        if (cnxnFactory != null) {
             cnxnFactory.shutdown();
         }
         removeDataDirectoryIfNecessary(dataDirectoryName, manageDataDir);
