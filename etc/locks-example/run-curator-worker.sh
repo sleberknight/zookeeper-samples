a0eb18e275144d2f4d11f1b86480c8072d3912c4
Scott Leberknight <scott.leberknight@nearinfinity.com>
2013-10-08 23:35:37 -0400

    Added Curator to project. Add CuratorWorker example showing how to use Curator's distributed lock recipe via the InterProcessMutex class. Added scripts to run various example locks with Curator.

---
+++ etc/locks-example/run-curator-worker.sh
@@ -0,0 +1,9 @@
+# This script takes one argument, a name to identity the client.
+# It runs the code in the com.nearinfinity.examples.zookeeper.lock.CuratorWorker class,
+# which uses Curator's InterProcessMutex distributed lock.
+
+# Arguments:
+# $1 - name
+# $2 - optional max wait time in seconds
+
+java -cp ../../target/classes:../../etc/examples-libs/* com.nearinfinity.examples.zookeeper.lock.CuratorWorker localhost:2181 /curator-lock $1 $2 &
---
+++ src/main/java/com/nearinfinity/examples/zookeeper/lock/CuratorWorker.java
@@ -0,0 +1,60 @@
+package com.nearinfinity.examples.zookeeper.lock;
+
+import java.util.concurrent.TimeUnit;
+
+import com.nearinfinity.examples.zookeeper.util.RandomAmountOfWork;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+public class CuratorWorker {
+
+    static final long DEFAULT_WAIT_TIME_SECONDS = Long.MAX_VALUE;
+
+    public static void main(String[] args) throws Exception {
+        String hosts = args[0];
+        String lockPath = args[1];
+        String myName = args[2];
+
+        long waitTimeSeconds = DEFAULT_WAIT_TIME_SECONDS;
+        if (args.length == 4) {
+            waitTimeSeconds = Long.valueOf(args[3]);
+        }
+
+        int baseSleepTimeMills = 1000;
+        int maxRetries = 3;
+
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(baseSleepTimeMills, maxRetries);
+        CuratorFramework client = CuratorFrameworkFactory.newClient(hosts, retryPolicy);
+        client.start();
+
+        InterProcessMutex lock = new InterProcessMutex(client, lockPath);
+        if (lock.acquire(waitTimeSeconds, TimeUnit.SECONDS)) {
+            try {
+                doSomeWork(myName);
+            } finally {
+                lock.release();
+            }
+        } else {
+            System.err.printf("%s timed out after %d seconds waiting to acquire lock on %s\n",
+                    myName, waitTimeSeconds, lockPath);
+        }
+
+        client.close();
+    }
+
+    private static void doSomeWork(String name) {
+        int seconds = new RandomAmountOfWork().timeItWillTake();
+        long workTimeMillis = seconds * 1000;
+        System.out.printf("%s is doing some work for %d seconds\n", name, seconds);
+        try {
+            Thread.sleep(workTimeMillis);
+        } catch (InterruptedException ex) {
+            System.out.printf("Oops. Interrupted.\n");
+            Thread.currentThread().interrupt();
+        }
+    }
+
+}
